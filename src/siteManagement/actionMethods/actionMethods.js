710b314737afdb694a520ee5c2e400ac24a73ac0
harsh1305 <harshshah130597@gmail.com>
2021-05-28 12:57:54 +0530

    fix: footfall chart api changes

--- src/siteManagement/actionMethods/actionMethods.js
+++ src/siteManagement/actionMethods/actionMethods.js
@@ -35,9 +35,9 @@ export function getSiteAreaIndex(requestBody) {
         .then(res => res.data).catch(err => err)
 }
 
-export const areaIndexChart = async (requestBody) => {
+export const footfallChart = async (requestBody) => {
     try {
-        const res = await axios.get(`${prefixURL}/get_site_area_index?session=${userSession}&org_id=${org_id}&date=${requestBody.date}&location_id=${requestBody.locationID}`);
+        const res = await axios.get(`${prefixURL}/get_site_footfall?session=${userSession}&org_id=${org_id}&date=${requestBody.date}&location_id=${requestBody.locationID}`);
         return res.data;
     } catch (err) {
         return err;
